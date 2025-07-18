9e525593e10792e443a3154ea42f8be4fbd23ae8
ctykk <ctyKK@outlook.com>
2025-07-18 14:24:53 +0800

    添加 util - tests 并发测试多个节点的有效性

--- ss_pool/util.py
+++ ss_pool/util.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import re
+from asyncio import as_completed, create_task, gather
 from base64 import b64decode
 from collections import defaultdict
 from typing import Collection
@@ -99,3 +100,29 @@ async def test(proxy: Proxy, session: ClientSession | None = None, timeout: floa
     finally:
         if flag:
             await session.close()
+
+
+async def tests(
+    *proxies: Proxy, session: ClientSession | None = None, timeout: float = 30
+) -> dict[Proxy, bool]:
+    """并发测试多个代理的有效性"""
+    result: dict[Proxy, bool] = dict()
+
+    async def _run(proxy: Proxy, session: ClientSession, timeout: float) -> tuple[Proxy, bool]:
+        return proxy, await test(proxy, session, timeout)
+
+    flag = False
+    # session 为 None，就创建新 session，并在结束时关闭该新 session
+    if session is None:
+        flag = True
+        session = ClientSession()
+
+    try:
+        tasks = [create_task(_run(p, session, timeout)) for p in proxies]
+        for p, r in await gather(*tasks):
+            result[p] = r
+    finally:
+        if flag:
+            await session.close()
+
+    return result
